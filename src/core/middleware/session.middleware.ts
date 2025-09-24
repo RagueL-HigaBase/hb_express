bc7f05fc80f1822cd2e6f3970cd2ca6b890b0dcf
RagueL-HigaBase <sergej.gnl@gmail.com>
2025-09-24 16:43:01 +0200

    name_change

--- src/core/middleware/session.middleware.ts
+++ src/core/middleware/session.middleware.ts
@@ -2,7 +2,7 @@ import type { NextFunction, Request, Response } from "express";
 import { sessionServicePatch } from "../services/update/session.service.js";
 import { sessionActive } from "../../shared/messages/server.js";
 
-export async function sessionMiddleware(req: Request, res: Response, next: NextFunction) {
+export async function loginMiddleware(req: Request, res: Response, next: NextFunction) {
     const { session } = req.cookies;
     if (!session || typeof session !== 'string') return next();
     const r = await sessionServicePatch(session);
--- src/core/routes/main.routes.ts
+++ src/core/routes/main.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { registerController } from "../controllers/create/register.controller.js";
 import { loginControllerCreate } from "../controllers/create/login.controller.js";
-import { sessionMiddleware } from "../middleware/session.middleware.js";
+import { loginMiddleware } from "../middleware/session.middleware.js";
 
 export const publicRouter = Router();
 
 publicRouter.post('/register', registerController);
 
-publicRouter.post('/login', sessionMiddleware, loginControllerCreate);
+publicRouter.post('/login', loginMiddleware, loginControllerCreate);
